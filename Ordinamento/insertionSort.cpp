ac7a371b4258077e8bf07c88179e6474596eb109
MarcoBertocco <891328@stud.unive.it>
2025-03-21 18:20:19 +0100

    Insertion Recursive

--- Ordinamento/insertionSort.cpp
+++ Ordinamento/insertionSort.cpp
@@ -16,13 +16,39 @@ void insertion_sort(vector<int> &A) // O(n^2)
     }
 }
 
+void insertion_sort_rec_aux(vector<int> &A, int dim)
+{
+    if (dim <= 0)
+        return;
+
+    insertion_sort_rec_aux(A, dim - 1);
+
+    int last = A[dim];
+    int j = dim - 1;
+
+    while (j >= 0 && A[j] > last)
+    {
+        A[j + 1] = A[j];
+        j--;
+    }
+    A[j + 1] = last;
+}
+
+void insertion_sort_rec(vector<int> &A)
+{
+    insertion_sort_rec_aux(A, A.size() - 1);
+}
+
 int main()
 {
     int arr[] = {100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5};
     vector<int> vettore(arr, arr + sizeof(arr) / sizeof(arr[0]));
 
     // Complessità di Insertion:O(n^2)
-    insertion_sort(vettore);
+    // insertion_sort(vettore);
+
+    insertion_sort_rec(vettore);
+
     for (int i = 0; i < vettore.size(); i++)
         cout << vettore.at(i) << " ";
     cout << endl;
