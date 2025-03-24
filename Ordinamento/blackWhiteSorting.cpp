c05f3413e990b9c158ce744a6e3ce1390008aff0
MarcoBertocco <891328@stud.unive.it>
2025-03-24 20:47:48 +0100

    blackWhite Sorting V2

--- Ordinamento/blackWhiteSorting.cpp
+++ Ordinamento/blackWhiteSorting.cpp
@@ -17,6 +17,36 @@ struct ColoredNumber
     ColoredNumber(int _k, Color _color) : k(_k), bw(_color) {}
 };
 
+void sort_by_color_merge(vector<ColoredNumber> &A, int left, int mid, int right)
+{
+    vector<ColoredNumber> temp;
+    int i = left, j = mid + 1;
+
+    while (i <= mid && A[i].bw == WHITE)
+        temp.push_back(A[i++]);
+    while (j <= right && A[j].bw == WHITE)
+        temp.push_back(A[j++]);
+
+    while (i <= mid)
+        temp.push_back(A[i++]);
+    while (j <= right)
+        temp.push_back(A[j++]);
+
+    for (int k = 0; k < temp.size(); k++)
+        A[left + k] = temp[k];
+}
+
+void sort_by_color_merge_sort(vector<ColoredNumber> &A, int left, int right)
+{
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+    sort_by_color_merge_sort(A, left, mid);
+    sort_by_color_merge_sort(A, mid + 1, right);
+    sort_by_color_merge(A, left, mid, right);
+}
+
 void sortbyColor(vector<ColoredNumber> &A)
 {
     int j = 0;
@@ -30,9 +60,6 @@ void sortbyColor(vector<ColoredNumber> &A)
         {
             if (j < i && A[i].bw == WHITE)
             {
-                // ColoredNumber tmp = A[i];
-                //  shift a dx gli elementi
-                // inserisco in A[j] A[i]
                 for (int x = i; x > j; x--)
                 {
                     swap(A[x], A[x - 1]);
@@ -55,9 +82,9 @@ int main()
     }
     cout << "]" << endl;
 
-    sortbyColor(vettore);
+    sort_by_color_merge_sort(vettore, 0, vettore.size() - 1);
 
-        cout << "[ ";
+    cout << "[ ";
     for (const auto &num : vettore)
     {
         cout << num.k << (num.bw == WHITE ? "W " : "B ");
