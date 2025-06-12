a76ea521dfed91db4c121ff5d313c6458c7ff002
MarcoBertocco <891328@stud.unive.it>
2025-06-12 10:49:19 +0200

    Visits Main Update

--- Strutture Dati/Tree/Algorithms/Visit/main.cpp
+++ Strutture Dati/Tree/Algorithms/Visit/main.cpp
@@ -6,13 +6,25 @@
 */
 int main()
 {
-    Tree root = new Node("A");
+    /*Tree root = new Node("A");
     root->left = new Node("L");
     root->right = new Node("B");
     root->left->left = new Node("E");
     root->left->right = new Node("R");
     root->right->left = new Node("O");
-    root->right->right = new Node("!");
+    root->right->right = new Node("!");*/
+
+    Node* root = new Node("20");
+    root->left = new Node("18");
+    root->right = new Node("30");
+
+    root->left->right = new Node("13");
+    root->left->right->left = new Node("14");
+
+    root->right->left = new Node("5");
+    root->right->right = new Node("9");
+    root->right->left->right = new Node("7");
+
 
     std::cout << "GENERIC: ";
     generic_visit(root);
