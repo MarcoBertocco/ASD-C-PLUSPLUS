0c9026e31a3c079c974b81520da48fad9ce69289
MarcoBertocco <891328@stud.unive.it>
2025-07-10 12:22:57 +0200

    ThreePosExist Problem

---
+++ Ordinamento/Esercizi/threePosExist.cpp
@@ -0,0 +1,105 @@
+// Sia A un array di n numeri interi. Si consideri il problema di decidere se esistono 3 posizioni distinte
+// x, y, z in A tali che A[x] + A[y] + A[z] = 0.
+// Scrivere un algoritmo di complessità O(n^2 ) per risolvere il problema.
+// (Suggerimento: si ordini A, poi si utilizzino 3 contatori i, j, k: i assume tutti i valori da 1 a n^2; poi per
+// ogni valore di i, j viene inizializzato a i + 1, e k ad n ...).
+// Dimostrare che la complessità della soluzione proposta sia O(n^2 ).
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define Near_infinite 2147483647
+void merge(vector<int> &A, int p, int q, int r) // O(n)
+{
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
+    vector<int> L(n1 + 1), R(n2 + 1);
+
+    for (int i = 0; i < n1; i++) // O(n1)
+        L[i] = A[p + i];
+    for (int j = 0; j < n2; j++) // O(n2)
+        R[j] = A[q + j + 1];
+
+    L[n1] = Near_infinite;
+    R[n2] = Near_infinite;
+
+    int i = 0, j = 0;
+    for (int k = p; k <= r; k++) // O(n)
+    {
+        if (L[i] <= R[j])
+        {
+            A[k] = L[i];
+            i++;
+        }
+        else
+        {
+            A[k] = R[j];
+            j++;
+        }
+    }
+}
+void merge_sort(vector<int> &A, int p, int r) // 2T(n/2) + O(n) -> O(nlog(n))
+{
+    if (p < r)
+    {
+        int q = (p + r) / 2;
+        merge_sort(A, p, q);
+        merge_sort(A, q + 1, r);
+        merge(A, p, q, r); // O(n)
+    }
+}
+
+bool twoSumRec(vector<int> &A, int i, int j, int k, int n) {
+    if (j >= k) return false;
+
+    int sum = A[i] + A[j] + A[k];
+    if (sum == 0) return true;
+    else if (sum < 0) return twoSumRec(A, i, j + 1, k, n);
+    else return twoSumRec(A, i, j, k - 1, n);
+}
+
+bool threePos1Rec(vector<int> &A, int i, int n) {
+    if (i >= n - 2) return false; // base case
+    if (twoSumRec(A, i, i + 1, n - 1, n)) return true;
+    return threePos1Rec(A, i + 1, n); // try next i
+}
+
+bool threePos1(vector<int> A, int n) {
+    merge_sort(A, 0, n - 1); // or std::sort(A.begin(), A.end());
+    
+    return threePos1Rec(A, 0, n);
+}
+
+// solution
+bool threePos(vector<int> A, int n)
+{
+    merge_sort(A, 0, n - 1);
+
+    bool end = false;
+    for (int i = 0; i < n - 2; ++i)
+    {
+        int j = i + 1;
+        int k = n - 1;
+
+        while (j < k && !end)
+        {
+            int sum = A[i] + A[j] + A[k];
+            if (sum == 0)
+                end = true;
+            else if (sum < 0)
+                j++;
+            else
+                k--;
+        }
+    }
+
+    return end;
+}
+int main()
+{
+    vector<int> A = {-7, -5, -3, -1, 0, 1, 4, 6};
+    cout << "Esiste? " << ((threePos(A, A.size())) ? "YES" : "NO") << endl;
+    cout << "Esiste? " << ((threePos1(A, A.size())) ? "YES" : "NO") << endl;
+}
